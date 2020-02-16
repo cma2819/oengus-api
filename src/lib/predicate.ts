b5edb8f01d843ce5196e13aa9a8b405d4c26999b
cma-desktop <yamanaka-team@hotmail.co.jp>
2020-02-16 14:09:10 +0900

    Fix error parsing Marathon

--- src/lib/predicate.ts
+++ src/lib/predicate.ts
@@ -1,31 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { OengusMarathon, OengusQuestion, OengusUser, OengusRole, OengusFieldType } from '../types';
 
 export const isRole = (source: any): source is OengusRole => {
     if (typeof source !== 'string') {
+        console.error(`Role[${source}] could not be parsed.`);
         return false;
     }
-    return ['SINGLE', 'RACE', 'COOP', 'OTHER'].find((type: string) => {
+    if (['ROLE_USER'].find((type: string) => {
         return type === source;
-    }) !== undefined;
+    }) === undefined) {
+        console.error(`Role[${source}] could not be parsed.`);
+        return false;
+    }
+    return true;
 }
 export const isUser = (source: any): source is OengusUser => {
     if (
         !source.id || typeof source.id !== 'number' ||
         !source.username || typeof source.username !== 'string' ||
         (source.usernameJapanese && typeof source.usernameJapanese !== 'string') ||
-        source.enabled !== undefined || typeof source.enabled !== 'boolean' ||
+        source.enabled === undefined || typeof source.enabled !== 'boolean' ||
         (source.twitterName && typeof source.twitterName !== 'string') ||
         (source.twitchName && typeof source.twitchName !== 'string') ||
         (source.speedruncomName && typeof source.speedruncomName !== 'string') ||
         source.atLeastOneAccountSynchronized === undefined || typeof source.atLeastOneAccountSynchronized !== 'boolean' ||
         source.emailPresentForExistingUser === undefined || typeof source.emailPresentForExistingUser !== 'boolean'
     ) {
+        console.error(`User[${source.id}] could not be parsed.`);
         return false;
     }
     if (!source.roles || typeof source.roles !== 'object'
         || source.roles.find((ele: any) => {
             return !isRole(ele);
         }) !== undefined) {
+        console.error(`User[${source.id}] could not be parsed.`);
         return false;
     }
     return true;
@@ -33,16 +41,21 @@ export const isUser = (source: any): source is OengusUser => {
 
 export const isFieldType = (source: any): source is OengusFieldType => {
     if (typeof source !== 'string') {
+        console.error(`FieldType[${source}] could not be parsed.`);
         return false;
     }
-    return ['TEXT', 'SELECT', 'TEXTATRA', 'CHECKBOX', 'FREETEXT'].find((type: string) => {
+    if (['TEXT', 'SELECT', 'TEXTATRA', 'CHECKBOX', 'FREETEXT'].find((type: string) => {
         return type === source;
-    }) !== undefined;
+    }) === undefined) {
+        console.error(`FieldType[${source}] could not be parsed.`);
+        return false;
+    }
+    return true;
 }
 
 export const isQuestion = (source: any): source is OengusQuestion => {
     if (
-        !source.id || typeof source.id !== 'string' ||
+        !source.id || typeof source.id !== 'number' ||
         !source.label || typeof source.label !== 'string' ||
         !source.fieldType || !isFieldType(source.fieldType) ||
         source.required === undefined || typeof source.required !== 'boolean' ||
@@ -50,12 +63,15 @@ export const isQuestion = (source: any): source is OengusQuestion => {
         (source.description && typeof source.description !== 'string') ||
         !source.position || typeof source.position !== 'number'
     ) {
+        console.error(`Question[${source.id}] could not be parsed.`);
         return false;
     }
     if (!source.options || typeof source.options !== 'object'
         || source.options.find((ele: any) => {
             return typeof ele !== 'string';
         }) !== undefined) {
+            console.log(typeof source.options);
+        console.error(`Question[${source.id}] could not be parsed.`);
         return false;
     }
     return true;
@@ -95,18 +111,21 @@ export const isMarathon = (source: any): source is OengusMarathon => {
         (source.donationsTotal && typeof source.donationsTotal !== 'number') ||
         (source.hasSubmitted === undefined && typeof source.hasSubmitted !== 'boolean')
     ) {
+        console.error(`Marathon[${source.id}] could not be parsed.`);
         return false;
     }
     if (!source.moderators || typeof source.moderators !== 'object'
         || source.moderators.find((ele: any) => {
             return !isUser(ele);
         }) !== undefined) {
+        console.error(`Marathon[${source.id}] could not be parsed.`);
         return false;
     }
     if (!source.questions || typeof source.questions !== 'object'
         || source.questions.find((ele: any) => {
             return !isQuestion(ele);
         }) !== undefined) {
+        console.error(`Marathon[${source.id}] could not be parsed.`);
         return false;
     }
     return true
